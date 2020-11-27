72512dc116e989823a569afcb5c7d31abdf183f9
shreyashetty207 <74947169+shreyashetty207@users.noreply.github.com>
2020-11-27 21:52:46 +0530

    Add files via upload

---
+++ Task 5/prb 1.py
@@ -0,0 +1,8 @@
+def op(a,b):
+    print(" Addition of two numbers  is ",a+b)
+    print("SUbtraction of two number is ",a-b)
+    print("Multiplication of two number is ",a*b)
+    print("Division of two number is",a//b)
+x=int(input("First number="))
+y=int(input("second number="))
+op(x,y)
---
+++ Task 5/prb 2.py
@@ -0,0 +1,6 @@
+def covid(name,temp=98):
+    print("Patient name :",name)
+    print("Temperature:",temp)
+a = input("Enter patient name -")
+b = input("Enter body  temperature -")
+covid(a,b)
