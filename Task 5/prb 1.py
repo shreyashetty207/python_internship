a9cae2b2ea2d84e9057627bef0236a730511a0c7
shreyashetty207 <74947169+shreyashetty207@users.noreply.github.com>
2020-11-27 23:50:35 +0530

    Update prb 1.py

--- Task 5/prb 1.py
+++ Task 5/prb 1.py
@@ -1,3 +1,8 @@
+#Create a function getting two integer inputs from user. & print the following:
+#Addition of two numbers is +value
+#Subtraction of two numbers is +value
+#Division of two numbers is +value
+#Multiplication of two numbers is +value
 def op(a,b):
     print(" Addition of two numbers  is ",a+b)
     print("SUbtraction of two number is ",a-b)
